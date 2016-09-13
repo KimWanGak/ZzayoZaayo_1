15a9a5dffe61597483501657e8e48ca456c423f1
KimWanGak <hgoosos@naver.com>
2016-09-13 13:58:06 +0900

    Create TestProj.java

---
+++ TestProj.java
@@ -0,0 +1,11 @@
+public void TestProj{ //
+	public static final NAME = "KimWanGak";
+	public static void main(String[] args){
+		
+	
+	System.out.println("Hi~ Every One My name is "+NAME);
+	
+	}
+
+
+}
